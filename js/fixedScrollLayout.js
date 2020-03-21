45e89229d1532b12271fa05a863f26b14b61738f
MrBahur <MrBahur25@gmail.com>
2020-03-21 22:27:59 +0200

    did alot of changes, forgot to commit

--- js/fixedScrollLayout.js
+++ js/fixedScrollLayout.js
@@ -14,7 +14,7 @@ var fixedScrollLayout = (function() {
 		// the body animation speed
 		animspeed : 650,
 		// the body animation easing (jquery easing)
-		animeasing : 'easeInOutExpo'
+		animeasing : 'easeInOutQuart'
 	};
 
 	function init() {
@@ -58,3 +58,25 @@ var fixedScrollLayout = (function() {
 	return { init : init };
 
 })();
+
+const els = document.querySelectorAll("[type='radio']");
+for (const el of els)
+  el.addEventListener("input", e => reorder(e.target, els));
+reorder(els[0], els);
+
+function reorder(targetEl, els) {
+  const nItems = els.length;
+  let processedUncheck = 0;
+  for (const el of els) {
+    const containerEl = el.nextElementSibling;
+    if (el === targetEl) {//checked radio
+      containerEl.style.setProperty("--w", "100%");
+      containerEl.style.setProperty("--l", "0");
+    }
+    else {//unchecked radios
+      containerEl.style.setProperty("--w", `${100/(nItems-1)}%`);
+      containerEl.style.setProperty("--l", `${processedUncheck * 100/(nItems-1)}%`);
+      processedUncheck += 1;
+    }
+  }
+}
